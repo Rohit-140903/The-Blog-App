d79f2a325f92c92c12030cd89e16ee24ac3bf2d4
Rohit Kumar Shaw <swrohit2003@gmail.com>
2024-07-20 12:59:29 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Logo({width = '100px'}){
+    return (
+        <div>
+            <img className = 'w-full h-20 object-contain' src = "https://cdn-icons-png.flaticon.com/128/4104/4104910.png"/>
+        </div>
+    )
+}
+export default Logo
---
+++ src/components/index.js
@@ -0,0 +1,38 @@
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import Container from './Container/Container';
+import Logo from './Logo';
+import LogoutBtn from './Header/Logout';
+import Button from "./Button";
+import Input  from "./Input";
+import PostForm from "./Post-form/PostForm";
+import AuthLayout from "./AuthLayout";
+import Signup from "./Signup";
+import Login from "./Login";
+import Select from "./Select";
+import PostCard from "./PostCard"
+import RTE from "./RTE";
+
+
+
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,
+    Input,
+    PostCard,
+    AuthLayout,
+    Login,
+    Signup,
+    Select,
+    PostForm,
+    RTE,
+
+
+
+    
+}
