2a1fdd151c4cb0d9c13f097215a427957c665bc8
Rohit Kumar Shaw <swrohit2003@gmail.com>
2024-08-04 21:17:00 +0530

    RTE plugin changes

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -16,8 +16,12 @@ export default function RTE({name, control, label, defaultValue =""}) {
     <Editor
       apiKey='sau8y7wp48liemb22v7mcouxzenmcjmj7f1becz6tgdcxo4q'
       init={{
-        plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
-        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+        // plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
+        // toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+        plugins: [ 'table powerpaste',
+          'lists media',
+          'paste' ],
+        toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify',
         tinycomments_mode: 'embedded',
         tinycomments_author: 'Author name',
         mergetags_list: [
